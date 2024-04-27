1a85fb99c9d227735f5958f9bfe480d3012ce74a
treytipton <treytipto@outlook.com>
2024-04-27 13:37:32 -0500

    Changed header to bootstrap navbar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { NavbarComponent } from '../navbar/navbar.component';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
