b3c9448a602724d3cd667a099694643503e2503b
github.com-natsch2020 <natsch2020@gmail.com>
2024-05-04 18:26:15 -0500

    Installed mongoose, updated the tsconfig and foot
    Used npm install mongoose
    Updated tsconfig so that no red squigglies appear under every @module
    Updated footer.html so that it fits the project requirements

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { User } from '../models/user.model';
-import { Post } from '../models/post.model';
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
+
 export class ProfileComponent {
   demoUser: User = {
     userID: "1508",
