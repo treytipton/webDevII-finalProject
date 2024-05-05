27450cd5cb532e91a5722ff43bc7c970e3576ca2
Ty Hastings <hastingsty44@gmail.com>
2024-05-04 19:05:28 -0500

    Removed mock data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -37,7 +37,7 @@ export class HomeComponent implements OnInit, OnDestroy {
       title: form.value.title,
       content: form.value.content
     }
-    this.demoPosts.push(newPost)
+    //this.demoPosts.push(newPost)
   }
 
 }
